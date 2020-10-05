b2ff680cc6e4a82e26c3f4efc976927ac3a00772
Rami Abdou <ramiabdou98@gmail.com>
2020-10-05 11:35:24 -0700

    Redirect from login working.

--- src/Router.tsx
+++ src/Router.tsx
@@ -20,7 +20,7 @@ import HomePage from '@scenes/Home/Home';
 import LoginPage from '@scenes/Login/Login';
 import SignupPage from '@scenes/Signup/Signup';
 import { useStoreActions } from '@store/Store';
-import { GET_USER } from '@store/UserGQL';
+import { GET_USER, IS_LOGGED_IN } from '@store/UserGQL';
 
 const AuthenticatedRoute = ({ component, ...rest }) => {
   const { loading, data } = useQuery(GET_USER);
@@ -32,15 +32,22 @@ const AuthenticatedRoute = ({ component, ...rest }) => {
   }, [data?.getUser?.id]);
 
   if (loading) return <Loader />;
-  if (data?.getUser) return <Route {...rest} component={component} />;
+  if (data?.getUser) return <Route exact {...rest} component={component} />;
   return <Redirect to="/login" />;
+};
+
+const LoginRoute = ({ component, ...rest }) => {
+  const { loading, data } = useQuery(IS_LOGGED_IN);
+  if (loading) return <Loader />;
+  if (data?.isUserLoggedIn) return <Redirect to="/" />;
+  return <Route {...rest} exact component={component} />;
 };
 
 export default () => (
   <Router>
     <Switch>
       <AuthenticatedRoute exact component={HomePage} path="/" />
-      <Route exact component={LoginPage} path="/login" />
+      <LoginRoute exact component={LoginPage} path="/login" />
       <Route exact component={SignupPage} path="/:community" />
     </Switch>
   </Router>
--- src/core/store/UserGQL.ts
+++ src/core/store/UserGQL.ts
@@ -9,3 +9,5 @@ export const GET_USER = query({
   fields: ['email', 'id', 'firstName', 'lastName'],
   operation: 'getUser'
 }).query;
+
+export const IS_LOGGED_IN = query({ operation: 'isUserLoggedIn' }).query;
