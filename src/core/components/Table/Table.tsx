d25217820811bee0fb3255e0c75aa7c1301cf920
Rami Abdou <ramiabdou98@gmail.com>
2020-11-23 09:31:37 -0800

    Updated table display properties.

--- src/core/components/Table/Table.tsx
+++ src/core/components/Table/Table.tsx
@@ -7,6 +7,7 @@ import './Table.scss';
 
 import React, { useEffect, useRef, useState } from 'react';
 
+import { useStoreState } from '@store/Store';
 import Body from './components/Body';
 import Header from './components/Header';
 import PaginationBar from './components/PaginationBar';
@@ -14,6 +15,7 @@ import PaginationBar from './components/PaginationBar';
 export default () => {
   const [height, setHeight] = useState(0);
   const ref: React.MutableRefObject<HTMLDivElement> = useRef(null);
+  const windowWidth = useStoreState(({ screen }) => screen.windowWidth);
 
   useEffect(() => {
     if (!ref?.current) return;
@@ -22,7 +24,7 @@ export default () => {
       window.screen.availHeight - (window.outerHeight - window.innerHeight);
 
     setHeight(maxScreenHeight - ref.current.offsetTop);
-  }, [ref]);
+  }, [ref, windowWidth]);
 
   return (
     <>
