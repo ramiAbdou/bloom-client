76e1ccc36c50cd7fc25e1f0b0d4e7da21b8f9786
Rami Abdou <ramiabdou98@gmail.com>
2020-11-24 16:18:22 -0800

    Component of icons.

--- src/core/components/Icons/ArrowUpCircle.tsx
+++ src/core/components/Icons/ArrowUpCircle.tsx
@@ -5,6 +5,10 @@
 
 import React from 'react';
 
-import arrowUpCircle from './images/arrow-up-circle.svg';
+import Icon from './Icon';
 
-export default () => <img className="react-icon" src={arrowUpCircle} />;
+export default () => (
+  <Icon>
+    <path d="M256 48C141.13 48 48 141.13 48 256s93.13 208 208 208 208-93.13 208-208S370.87 48 256 48zm91.36 212.65a16 16 0 01-22.63.09L272 208.42V342a16 16 0 01-32 0V208.42l-52.73 52.32A16 16 0 11164.73 238l80-79.39a16 16 0 0122.54 0l80 79.39a16 16 0 01.09 22.65z" />
+  </Icon>
+);
