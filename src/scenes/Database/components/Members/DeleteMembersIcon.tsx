0a01ff2c29e320d312c1b78429150f76f9cbc665
Rami Abdou <ramiabdou98@gmail.com>
2020-11-26 11:15:54 -0800

    Delete members modal done.

--- src/scenes/Database/components/Members/DeleteMembersIcon.tsx
+++ src/scenes/Database/components/Members/DeleteMembersIcon.tsx
@@ -6,22 +6,28 @@
 import { useMutation } from 'graphql-hooks';
 import React from 'react';
 
+import OutlineButton from '@components/Button/OutlineButton';
+import PrimaryButton from '@components/Button/PrimaryButton';
 import Trash from '@components/Icons/Trash';
+import Modal from '@components/Modal/Modal';
 import Table from '@components/Table/Table.store';
 import { useStoreActions, useStoreState } from '@store/Store';
 import { DELETE_MEMBERSHIPS } from '../../Database.gql';
 import DatabaseAction from './DatabaseAction';
 
-export default () => {
-  const members = useStoreState((store) => store.community.members);
+const MODAL_ID = 'DELETE_MEMBERS';
+
+const DeleteMembersModal = () => {
+  const members = useStoreState(({ community }) => community.members);
+  const closeModal = useStoreActions(({ modal }) => modal.closeModal);
   const updateCommunity = useStoreActions((actions) => actions.updateCommunity);
   const membershipIds = Table.useStoreState(
     ({ selectedRowIds }) => selectedRowIds
   );
 
-  const [deleteMemberships] = useMutation(DELETE_MEMBERSHIPS);
+  const [deleteMemberships, { loading }] = useMutation(DELETE_MEMBERSHIPS);
 
-  const onClick = async () => {
+  const onDelete = async () => {
     const { data } = await deleteMemberships({ variables: { membershipIds } });
     if (!data?.deleteMemberships) return;
 
@@ -33,6 +39,33 @@ export default () => {
   };
 
   return (
-    <DatabaseAction Component={Trash} value="Delete Member" onClick={onClick} />
+    <Modal confirmation id={MODAL_ID}>
+      <h1>Remove member(s)?</h1>
+      <p>
+        Are you sure you want to remove these member(s)? They will no longer
+        have access to your community and they will not show up in the member
+        database.
+      </p>
+      <div>
+        <PrimaryButton loading={loading} title="Remove" onClick={onDelete} />
+        <OutlineButton title="Cancel" onClick={closeModal} />
+      </div>
+    </Modal>
+  );
+};
+
+export default () => {
+  const showModal = useStoreActions(({ modal }) => modal.showModal);
+  const onClick = () => showModal({ id: MODAL_ID });
+
+  return (
+    <>
+      <DeleteMembersModal />
+      <DatabaseAction
+        Component={Trash}
+        value="Delete Member"
+        onClick={onClick}
+      />
+    </>
   );
 };
--- src/scenes/Database/components/Members/PromoteToAdminIcon.tsx
+++ src/scenes/Database/components/Members/PromoteToAdminIcon.tsx
@@ -16,16 +16,18 @@ import { useStoreActions } from '@store/Store';
 import { PROMOTE_TO_ADMIN } from '../../Database.gql';
 import DatabaseAction from './DatabaseAction';
 
-export default () => {
+const MODAL_ID = 'CONFIRM_PROMOTE_TO_ADMIN';
+
+const PromoteToAdminModal = () => {
   const closeModal = useStoreActions(({ modal }) => modal.closeModal);
   const setOnClose = useStoreActions(({ modal }) => modal.setOnClose);
-  const showModal = useStoreActions(({ modal }) => modal.showModal);
   const membershipIds = Table.useStoreState(
     ({ selectedRowIds }) => selectedRowIds
   );
 
   const { push } = useHistory();
   const [promoteToAdmin, { loading }] = useMutation(PROMOTE_TO_ADMIN);
+
   const onPromote = async () => {
     setOnClose(() => push('admins'));
     const { data } = await promoteToAdmin({ variables: { membershipIds } });
@@ -33,30 +35,34 @@ export default () => {
     closeModal();
   };
 
-  const MODAL_ID = 'CONFIRM_PROMOTE_TO_ADMIN';
+  return (
+    <Modal confirmation id={MODAL_ID}>
+      <h1>Promote to admin?</h1>
+      <p>
+        Are you sure you want to promote this member to admin? They will be
+        granted all admin priviledges. You can undo this action at any time.
+      </p>
+      <div>
+        <PrimaryButton loading={loading} title="Promote" onClick={onPromote} />
+        <OutlineButton title="Cancel" onClick={closeModal} />
+      </div>
+    </Modal>
+  );
+};
+
+export default () => {
+  const showModal = useStoreActions(({ modal }) => modal.showModal);
+  const disabled = Table.useStoreState(
+    ({ selectedRowIds }) => selectedRowIds.length > 15
+  );
   const onClick = () => showModal({ id: MODAL_ID });
 
   return (
     <>
-      <Modal confirmation id={MODAL_ID}>
-        <h1>Promote to admin?</h1>
-        <p>
-          Are you sure you want to promote this member to admin? They will be
-          granted all admin priviledges. You can undo this action at any time.
-        </p>
-        <div>
-          <PrimaryButton
-            loading={loading}
-            title="Promote"
-            onClick={onPromote}
-          />
-          <OutlineButton title="Cancel" onClick={closeModal} />
-        </div>
-      </Modal>
-
+      <PromoteToAdminModal />
       <DatabaseAction
         Component={ArrowUpCircle}
-        disabled={membershipIds.length > 15}
+        disabled={disabled}
         value="Promote to Admin"
         onClick={onClick}
       />
