ef7dac3a7ab979bdcb77cb1e5a7558f0aff47966
Rami Abdou <ramiabdou98@gmail.com>
2020-12-09 12:25:26 -0800

    Auto fill grid layout for the directory.

--- src/core/components/Modal/ModalContainer.tsx
+++ src/core/components/Modal/ModalContainer.tsx
@@ -2,7 +2,6 @@ import { motion } from 'framer-motion';
 import React, { useCallback, useEffect } from 'react';
 
 import { ChildrenProps } from '@constants';
-import useBreakpoint from '@hooks/useBreakpoint';
 import useLockBodyScroll from '@hooks/useLockBodyScroll';
 
 interface ModalContainerProps extends ChildrenProps {
@@ -11,13 +10,7 @@ interface ModalContainerProps extends ChildrenProps {
 }
 
 export default ({ children, onClose, width }: ModalContainerProps) => {
-  const isMobile = useBreakpoint() === 'M';
-
   useLockBodyScroll();
-
-  const animate = isMobile ? { x: 0 } : { opacity: 1, scale: 1 };
-  const exit = isMobile ? { x: 1000 } : { opacity: 0, scale: 0.75 };
-  const initial = isMobile ? { x: 1000 } : { opacity: 0.25, scale: 0.5 };
 
   // We memoize the onClose function so that we don't continuously re-render.
   // The dep array is empty because the props will be present on component will
@@ -34,10 +27,10 @@ export default ({ children, onClose, width }: ModalContainerProps) => {
 
   return (
     <motion.div
-      animate={animate}
+      animate={{ opacity: 1, scale: 1 }}
       className="c-modal-ctr"
-      exit={exit}
-      initial={initial}
+      exit={{ opacity: 0, scale: 0.75 }}
+      initial={{ opacity: 0.25, scale: 0.5 }}
       style={width ? { width } : {}}
       transition={{ duration: 0.2 }}
     >
--- src/scenes/Directory/components/MemberCard/MemberCard.container.tsx
+++ src/scenes/Directory/components/MemberCard/MemberCard.container.tsx
@@ -9,15 +9,6 @@ import Directory from '../../Directory.store';
 import MemberCard from './MemberCard';
 import { MemberCardData } from './MemberCard.store';
 
-const randomPictures = [
-  'https://pbs.twimg.com/profile_images/1309512858951131138/8UACAdfa_400x400.jpg',
-  'https://pbs.twimg.com/profile_images/1303060784289730560/femQ8Zek_400x400.jpg',
-  'https://pbs.twimg.com/profile_images/1216728758473953281/HY15R6ER_400x400.jpg',
-  'https://pbs.twimg.com/profile_images/1322009883596726272/5lguqewe_400x400.jpg',
-  'https://pbs.twimg.com/profile_images/1285792980872429568/BkcFk2jp_400x400.jpg',
-  'https://pbs.twimg.com/profile_images/1289268330088595456/s-5tN4Oi_400x400.jpg'
-];
-
 export default () => {
   const searchString = Directory.useStoreState((store) => store.searchString);
 
@@ -59,8 +50,7 @@ export default () => {
           instagramUrl,
           lastName,
           linkedInUrl,
-          pictureUrl:
-            randomPictures[Math.round(Math.random() * 6)] ?? pictureUrl,
+          pictureUrl,
           twitterUrl
         };
       });
@@ -85,7 +75,6 @@ export default () => {
     <Masonry
       key={`${searchString}-${members?.length}`}
       className="s-directory-card-ctr"
-      columnCount={6}
       columnGutter={16}
       items={members ?? []}
       overscanBy={5}
