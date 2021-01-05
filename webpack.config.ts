888b5a0ee62e5892921179861b7ab0e2c5d4ea36
Rami Abdou <ramiabdou98@gmail.com>
2021-01-05 12:19:46 -0800

    Refactored webpack build.

--- webpack.config.ts
+++ webpack.config.ts
@@ -4,26 +4,15 @@ import Dotenv from 'dotenv-webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import path from 'path';
-import webpack from 'webpack';
+import StylelintPlugin from 'stylelint-webpack-plugin';
+import webpack, { Configuration } from 'webpack';
 
-export default {
+import { isProduction } from './src/util/constants';
+
+const baseConfig: Configuration = {
   entry: path.join(__dirname, '/src/App.tsx'),
-  mode: 'production',
   module: {
     rules: [
-      {
-        exclude: /node_modules/,
-        loader: 'ts-loader',
-        test: /\.(ts|tsx)$/
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader'
-          }
-        ]
-      },
       {
         test: /\.scss$/,
         use: [
@@ -37,20 +26,13 @@ export default {
       {
         exclude: /node_modules/,
         loader: 'file-loader',
-        options: {
-          context: 'src',
-          name: 'images/[name].[ext]'
-        },
-        test: /\.(jpg|jpeg|gif|png|svg|ico)$/
+        options: { context: 'src', name: 'images/[name].[ext]' },
+        test: /\.(png|svg|ico)$/
       },
       {
         exclude: /node_modules/,
-        loader: 'file-loader',
-        options: {
-          context: 'src',
-          name: 'documents/[name].[ext]'
-        },
-        test: /\.(pdf)$/
+        loader: 'ts-loader',
+        test: /\.(ts|tsx)$/
       }
     ]
   },
@@ -70,6 +52,7 @@ export default {
       template: path.join(__dirname, '/public/index.html')
     }),
     new MiniCssExtractPlugin(),
+    new StylelintPlugin({ fix: true }),
     new webpack.HotModuleReplacementPlugin()
   ],
   resolve: {
@@ -77,3 +60,29 @@ export default {
     plugins: [new TsConfigPathsPlugin()]
   }
 };
+
+const devConfig = {
+  devServer: {
+    disableHostCheck: true,
+    historyApiFallback: true,
+    hot: true,
+    open: true,
+    port: 3000
+  }
+};
+
+const config: Configuration = {
+  ...baseConfig,
+  ...(!isProduction ? devConfig : {}),
+  mode: isProduction ? 'production' : 'development',
+  module: {
+    rules: [
+      ...baseConfig.module.rules,
+      ...(isProduction
+        ? [{ test: /\.html$/, use: [{ loader: 'html-loader' }] }]
+        : [])
+    ]
+  }
+};
+
+export default config;
