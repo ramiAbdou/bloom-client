9d19acabfc02279dbf99586b67bf86b2894ced32
Rami Abdou <ramiabdou98@gmail.com>
2021-01-05 07:05:18 -0800

    Login card looks better

--- src/scenes/Login/LoginCard/LoginCard.tsx
+++ src/scenes/Login/LoginCard/LoginCard.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import validator from 'validator';
 
 import Separator from '@atoms/Separator';
 import Logo from '@components/Misc/Logo';
 import Network from '@components/Misc/Network';
-import LoginCardEmailForm from './EmailForm';
+import Form from '@organisms/Form/Form';
+import FormErrorMessage from '@organisms/Form/FormErrorMessage';
+import SubmitButton from '@organisms/Form/FormSubmitButton';
+import FormItem from '@organisms/Form/Item';
 import LoginCardGoogleContainer from './GoogleForm';
+import useSendLoginLink from './useSendLoginLink';
+
+const LoginCardEmailForm: React.FC = () => {
+  const sendLoginLink = useSendLoginLink();
+
+  return (
+    <Form className="s-login-email-form" onSubmit={sendLoginLink}>
+      <FormItem
+        required
+        category="EMAIL"
+        description="Or continue with your email address to receive a login link."
+        placeholder="Email"
+        type="SHORT_TEXT"
+        validate={(value: string) => validator.isEmail(value)}
+      />
+
+      <FormErrorMessage marginTop={0} />
+      <SubmitButton loadingText="Sending...">Send Login Link</SubmitButton>
+    </Form>
+  );
+};
 
 const LoginCardHeader: React.FC = () => (
   <div className="s-login-header">
