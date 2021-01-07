5cd0373631d6708fd9c67a80b4fcf86e46d64bd1
Rami Abdou <ramiabdou98@gmail.com>
2021-01-06 16:36:01 -0800

    Ballots.

--- src/components/modals/Payment/useUpdatePaymentMethod.tsx
+++ src/components/modals/Payment/useUpdatePaymentMethod.tsx
@@ -30,52 +30,52 @@ const useUpdatePaymentMethod = (): OnFormSubmit => {
     setErrorMessage,
     setIsLoading
   }: OnFormSubmitArgs) => {
-    // const city = items.find(({ title }) => title === 'City').value;
-    // const state = items.find(({ title }) => title === 'State').value;
-    // const postalCode = items.find(({ title }) => title === 'Zip Code').value;
+    const city = items.find(({ title }) => title === 'City').value;
+    const state = items.find(({ title }) => title === 'State').value;
+    const postalCode = items.find(({ title }) => title === 'Zip Code').value;
 
-    // const nameOnCard = items.find(({ title }) => title === 'Name on Card')
-    //   .value;
+    const nameOnCard = items.find(({ title }) => title === 'Name on Card')
+      .value;
 
-    // // Start the submit function by clearing the error message and set the
-    // // form state to loading.
-    // setErrorMessage(null);
-    // setIsLoading(true);
+    // Start the submit function by clearing the error message and set the
+    // form state to loading.
+    setErrorMessage(null);
+    setIsLoading(true);
 
-    // // Create the payment method via the Stripe SDK.
-    // const stripeResult = await stripe.createPaymentMethod({
-    //   billing_details: {
-    //     address: { city, postal_code: postalCode, state },
-    //     name: nameOnCard
-    //   },
-    //   card: elements.getElement(CardElement),
-    //   type: 'card'
-    // });
+    // Create the payment method via the Stripe SDK.
+    const stripeResult = await stripe.createPaymentMethod({
+      billing_details: {
+        address: { city, postal_code: postalCode, state },
+        name: nameOnCard
+      },
+      card: elements.getElement(CardElement),
+      type: 'card'
+    });
 
-    // // If the card information is incorrect or doesn't work for some reason,
-    // // show that error message.
-    // if (stripeResult.error) {
-    //   setErrorMessage(stripeResult.error.message);
-    //   setIsLoading(false);
-    //   return;
-    // }
+    // If the card information is incorrect or doesn't work for some reason,
+    // show that error message.
+    if (stripeResult.error) {
+      setErrorMessage(stripeResult.error.message);
+      setIsLoading(false);
+      return;
+    }
 
-    // // Create the actual subscription. Pass the MemberType ID to know what
-    // // Stripe price ID to look up, as well as the newly created IPaymentMethod
-    // // ID. That will be attached to the customer ID associated with the member.
-    // const { data: updateData, error: updateError } = await updatePaymentMethod({
-    //   paymentMethodId: stripeResult.paymentMethod.id
-    // });
+    // Create the actual subscription. Pass the MemberType ID to know what
+    // Stripe price ID to look up, as well as the newly created IPaymentMethod
+    // ID. That will be attached to the customer ID associated with the member.
+    const { data: updateData, error: updateError } = await updatePaymentMethod({
+      paymentMethodId: stripeResult.paymentMethod.id
+    });
 
-    // if (updateError) {
-    //   setErrorMessage(updateError);
-    //   setIsLoading(false);
-    //   return;
-    // }
+    if (updateError) {
+      setErrorMessage(updateError);
+      setIsLoading(false);
+      return;
+    }
 
-    // // Success! Update the member entity just in case the membership type
-    // // changed or their duesStatus changed.
-    // mergeEntities({ data: updateData, schema: Schema.MEMBER });
+    // Success! Update the member entity just in case the membership type
+    // changed or their duesStatus changed.
+    mergeEntities({ data: updateData, schema: Schema.MEMBER });
     setScreen(type === 'UPDATE_PAYMENT_METHOD' ? 'CONFIRMATION' : 'FINISH');
   };
 
