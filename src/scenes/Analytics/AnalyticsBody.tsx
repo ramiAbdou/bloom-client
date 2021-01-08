2c90686667c109f320b6c9ee67352923ea29a553
Rami Abdou <ramiabdou98@gmail.com>
2021-01-08 07:08:26 -0800

    Fetching.

--- src/scenes/Analytics/AnalyticsBody.tsx
+++ src/scenes/Analytics/AnalyticsBody.tsx
@@ -2,14 +2,22 @@ import React from 'react';
 import { Redirect, Route, Switch, useRouteMatch } from 'react-router-dom';
 
 import MainContent from '@containers/Main/MainContent';
-import useFetchDatabase from '../Database/useFetchDatabase';
+import useQuery from '@hooks/useQuery';
+import { GET_DATABASE } from '@scenes/Database/Database.gql';
+import { ICommunity } from '@store/entities';
+import { Schema } from '@store/schema';
 import DuesAnalytics from './DuesAnalytics/DuesAnalytics';
 import EventsAnalytics from './EventsAnalytics/Events';
 import MembersAnalytics from './MembersAnalytics/MembersAnalytics';
 
 const AnalyticsBody: React.FC = () => {
   const { url } = useRouteMatch();
-  useFetchDatabase();
+
+  useQuery<ICommunity>({
+    name: 'getDatabase',
+    query: GET_DATABASE,
+    schema: Schema.COMMUNITY
+  });
 
   return (
     <MainContent>
