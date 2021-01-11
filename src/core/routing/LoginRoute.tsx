deca3967d870fca2b394a6fed195e29274be2fa8
Rami Abdou <ramiabdou98@gmail.com>
2021-01-11 08:52:12 -0800

    Login route looks good.

--- src/core/routing/LoginRoute.tsx
+++ src/core/routing/LoginRoute.tsx
@@ -8,19 +8,16 @@ import { IS_LOGGED_IN } from './Router.gql';
 
 // We need to pass in the path instead of just setting it to /login here, so
 // that the Switch component in the router properly switches.
-
 const LoginRoute: React.FC<Pick<RouteProps, 'path'>> = ({ path }) => {
   // Check to see if the user is logged in (if they have tokens stored in the
   // httpOnly cookies), and if so, redirect them to the home page.
-  const { error, loading, data: isAuthenticated } = useQuery<boolean>({
+  const { loading, data: isLoggedIn } = useQuery<boolean>({
     name: 'isUserLoggedIn',
     query: IS_LOGGED_IN
   });
 
   if (loading) return <Loader />;
-  // If there is an error in the GraphQL query, whether the query structure
-  // was wrong or there the user wasn't authenticated for the request.
-  if (!error && isAuthenticated) return <Redirect to="/" />;
+  if (isLoggedIn) return <Redirect to="/" />;
   return <Route component={LoginPage} path={path} />;
 };
 
