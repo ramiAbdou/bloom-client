6be39c462b7a3ec2c68b55092d85dac850d75e24
Rami Abdou <ramiabdou98@gmail.com>
2021-01-13 08:53:07 -0800

    Profile onboarding done.

--- src/scenes/Profile/ProfileSocialCard.tsx
+++ src/scenes/Profile/ProfileSocialCard.tsx
@@ -1,18 +1,42 @@
 import React from 'react';
 
-import { HeaderTag } from '@atoms/Tags';
+import Button from '@atoms/Button';
 import Card from '@containers/Card/Card';
-import Row from '@containers/Row/Row';
+import { useStoreState } from '@store/Store';
+import socialMedia from './images/social-media.svg';
 import ProfileCardHeader from './ProfileCardHeader';
 
+const ProfileSocialOnboardingContainer: React.FC = () => {
+  const isSocialLinked: boolean = useStoreState(({ db }) => {
+    const { facebookUrl, instagramUrl, linkedInUrl, twitterUrl } = db.user;
+    return !!facebookUrl || !!instagramUrl || !!linkedInUrl || !!twitterUrl;
+  });
+
+  if (isSocialLinked) return null;
+
+  return (
+    <>
+      <img
+        alt="Social Media"
+        className="s-profile-card--social-empty-img"
+        src={socialMedia}
+      />
+
+      <Button primary>+ Link Social Media</Button>
+    </>
+  );
+};
+
 const ProfileSocialCard: React.FC = () => {
+  const isSocialLinked: boolean = useStoreState(({ db }) => {
+    const { facebookUrl, instagramUrl, linkedInUrl, twitterUrl } = db.user;
+    return !!facebookUrl || !!instagramUrl || !!linkedInUrl || !!twitterUrl;
+  });
+
   return (
     <Card className="s-profile-card--social">
-      <ProfileCardHeader canEdit title="Social Media" />
-
-      <Row>
-        <HeaderTag />
-      </Row>
+      <ProfileCardHeader canEdit={isSocialLinked} title="Social Media" />
+      <ProfileSocialOnboardingContainer />
     </Card>
   );
 };
