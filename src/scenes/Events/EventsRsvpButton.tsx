391ba0bd0628923aee9257ff616c4538b41a29c0
Rami Abdou <ramiabdou98@gmail.com>
2021-03-05 04:29:41 -0800

    Create event guest looks okay.

--- src/scenes/Events/EventsRsvpButton.tsx
+++ src/scenes/Events/EventsRsvpButton.tsx
@@ -2,13 +2,13 @@ import day from 'dayjs';
 import React from 'react';
 
 import Button, { ButtonProps } from '@atoms/Button/Button';
-import useMutation from '@hooks/useMutation';
 import { ToastOptions } from '@organisms/Toast/Toast.types';
-import { IEvent, IEventGuest } from '@store/Db/entities';
+import { IEvent } from '@store/Db/entities';
 import { Schema } from '@store/Db/schema';
 import { useStoreActions, useStoreState } from '@store/Store';
 import { ModalType } from '@util/constants';
-import { CreateEventGuestArgs, DeleteEventGuestArgs } from './Events.types';
+import { DeleteEventGuestArgs } from './Events.types';
+import useCreateEventGuest from './useCreateEventGuest';
 
 interface EventRsvpButtonProps extends Partial<Pick<ButtonProps, 'large'>> {
   eventId: string;
@@ -38,24 +38,7 @@ const EventRsvpButton: React.FC<EventRsvpButtonProps> = ({
     return event?.guests?.some((guestId: string) => guests.has(guestId));
   });
 
-  const [createEventGuest] = useMutation<IEventGuest, CreateEventGuestArgs>({
-    fields: [
-      'createdAt',
-      'id',
-      { event: ['id'] },
-      { member: ['id', 'firstName', 'lastName', 'pictureUrl'] },
-      { supporter: ['id', 'firstName', 'lastName'] }
-    ],
-    operation: 'createEventGuest',
-    schema: Schema.EVENT_GUEST,
-    types: {
-      email: { required: false },
-      eventId: { required: true },
-      firstName: { required: false },
-      lastName: { required: false }
-    },
-    variables: { eventId }
-  });
+  const createEventGuest = useCreateEventGuest();
 
   const onClick = async (
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>
@@ -67,7 +50,7 @@ const EventRsvpButton: React.FC<EventRsvpButtonProps> = ({
       return;
     }
 
-    await createEventGuest();
+    await createEventGuest({ eventId });
 
     const options: ToastOptions<boolean, DeleteEventGuestArgs> = {
       message: 'RSVP was registered.',
