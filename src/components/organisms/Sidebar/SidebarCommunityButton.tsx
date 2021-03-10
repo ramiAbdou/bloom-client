62076c29fa3e49198f60f21496ae603907e1e4e5
Rami Abdou <ramiabdou98@gmail.com>
2021-03-10 14:13:07 -0800

    Sidebar smaller components.

---
+++ src/components/organisms/Sidebar/SidebarCommunityButton.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+import { ICommunity, IMember } from '@store/Db/entities';
+import { useStoreState } from '@store/Store';
+import { IdProps } from '@util/constants';
+import { cx } from '@util/util';
+
+const SidebarCommunityButton: React.FC<IdProps> = ({ id: memberId }) => {
+  const communityId: string = useStoreState(({ db }) => db.community.id);
+
+  const { id, logoUrl, urlName }: ICommunity = useStoreState(({ db }) => {
+    const member: IMember = db.byMemberId[memberId];
+    return db.byCommunityId[member.community] ?? {};
+  }) as ICommunity;
+
+  const { push } = useHistory();
+
+  const onClick = () => {
+    if (id !== communityId) push(`/${urlName}`);
+  };
+
+  const css: string = cx('br-xs h-xl mb-sm p-2 w-xl o-nav-community', {
+    'o-nav-community--active': id === communityId
+  });
+
+  return (
+    <button className={css} type="button" onClick={onClick}>
+      <img className="br-xxs h-100 w-100" src={logoUrl} />
+    </button>
+  );
+};
+
+export default SidebarCommunityButton;
--- src/components/organisms/Sidebar/SidebarCommunityList.tsx
+++ src/components/organisms/Sidebar/SidebarCommunityList.tsx
@@ -1,50 +1,23 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
 
-import { ICommunity, IMember } from '@store/Db/entities';
+import { IMember, MemberStatus } from '@store/Db/entities';
 import { useStoreState } from '@store/Store';
-import { IdProps } from '@util/constants';
-import { cx, sortObjects } from '@util/util';
-
-const SidebarCommunityIcon: React.FC<IdProps> = ({ id: memberId }) => {
-  const communityId = useStoreState(({ db }) => db.community.id);
-
-  const { id, logoUrl, urlName } = useStoreState(({ db }) => {
-    const member: IMember = db.byMemberId[memberId];
-    return db.byCommunityId[member.community] ?? {};
-  }) as ICommunity;
-
-  const { push } = useHistory();
-
-  const isActive: boolean = id === communityId;
-
-  const onClick = () => {
-    if (!isActive) push(`/${urlName}`);
-  };
-
-  const css: string = cx('o-nav-community', {
-    'o-nav-community--active': isActive
-  });
-
-  return (
-    <button className={css} type="button" onClick={onClick}>
-      <img src={logoUrl} />
-    </button>
-  );
-};
+import { sortObjects } from '@util/util';
+import SidebarCommunityButton from './SidebarCommunityButton';
 
 const SidebarCommunityList: React.FC = () => {
   const memberIds: string[] = useStoreState(({ db }) => {
     return db.user?.members
       ?.map((memberId: string) => db.byMemberId[memberId])
+      ?.filter((member: IMember) => member.status === MemberStatus.ACCEPTED)
       ?.sort((a, b) => sortObjects(a, b, 'joinedAt', 'ASC'))
       ?.map((member: IMember) => member.id);
   });
 
   return (
-    <div className="o-nav-community-ctr">
+    <div className="f f-ac f-col px-xs py-sm o-nav-community-ctr">
       {memberIds?.map((id: string) => (
-        <SidebarCommunityIcon key={id} id={id} />
+        <SidebarCommunityButton key={id} id={id} />
       ))}
     </div>
   );
