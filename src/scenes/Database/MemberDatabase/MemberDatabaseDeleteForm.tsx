5c5fa92a3e4a6841623a74cb0238eb7e86d3dad1
Rami Abdou <ramiabdou98@gmail.com>
2021-04-13 18:28:17 -0700

    Delete looks better.

--- src/scenes/Database/MemberDatabase/MemberDatabaseDeleteForm.tsx
+++ src/scenes/Database/MemberDatabase/MemberDatabaseDeleteForm.tsx
@@ -1,30 +1,61 @@
 import React from 'react';
 
 import Form from '@components/organisms/Form/Form';
-import { OnFormSubmitFunction } from '@components/organisms/Form/Form.types';
+import {
+  OnFormSubmitArgs,
+  OnFormSubmitFunction
+} from '@components/organisms/Form/Form.types';
 import FormHeader from '@components/organisms/Form/FormHeader';
 import ModalConfirmationActions from '@components/organisms/Modal/ModalConfirmationActions';
 import TableStore from '@components/organisms/Table/Table.store';
-import useDeleteMembers from './useDeleteMembers';
+import { IMember } from '@core/db/db.entities';
+import { now } from '@util/util';
 
 const MemberDatabaseDeleteFormHeader: React.FC = () => {
-  const memberIds = TableStore.useStoreState(
+  const memberIds: string[] = TableStore.useStoreState(
     ({ selectedRowIds }) => selectedRowIds
   );
 
-  return (
-    <FormHeader
-      description="Are you sure you want to remove these member(s)? They will no longer have access to your community and they will not show up in the member database."
-      title={`Remove ${memberIds?.length} member(s)?`}
-    />
-  );
+  const title: string = `Remove ${memberIds?.length} member(s)?`;
+
+  const description: string =
+    'Are you sure you want to remove these member(s)? They will no longer have access to your community and they will not show up in the member database.';
+
+  return <FormHeader description={description} title={title} />;
 };
 
 const MemberDatabaseDeleteForm: React.FC = () => {
-  const deleteForm: OnFormSubmitFunction = useDeleteMembers();
+  const memberIds: string[] = TableStore.useStoreState(
+    ({ selectedRowIds }) => selectedRowIds
+  );
+
+  const onSubmit: OnFormSubmitFunction = async ({
+    closeModal,
+    gql,
+    setError,
+    showToast
+  }: OnFormSubmitArgs) => {
+    const { error } = await gql.updateMany(IMember, {
+      data: { deletedAt: now() },
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      where: { id: { _in: memberIds } }
+    });
+
+    if (error) {
+      setError('Failed to delete member(s). Please try again later.');
+      return;
+    }
+
+    closeModal();
+
+    showToast({
+      message: `${memberIds.length} member(s) removed from the community.`
+    });
+  };
 
   return (
-    <Form options={{ disableValidation: true }} onSubmit={deleteForm}>
+    <Form options={{ disableValidation: true }} onSubmit={onSubmit}>
       <MemberDatabaseDeleteFormHeader />
 
       <ModalConfirmationActions
--- src/util/constants.events.ts
+++ src/util/constants.events.ts
@@ -4,7 +4,6 @@ export enum MutationEvent {
   CREATE_EVENT_ATTENDEE_WITH_SUPPORTER = 'createEventAttendeeWithSupporter',
   CREATE_EVENT_GUEST_WITH_SUPPORTER = 'createEventGuestWithSupporter',
   DELETE_EVENT_GUEST = 'deleteEventGuest',
-  DELETE_MEMBERS = 'deleteMembers',
   INVITE_MEMBERS = 'inviteMembers',
   LOGOUT = 'logout',
   REMOVE_STRIPE_SUBSCRIPTION_ID = 'removeStripeSubscriptionId',
