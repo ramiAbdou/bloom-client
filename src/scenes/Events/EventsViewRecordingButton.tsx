f99f1450dea10ac0a797d036ccc9ce7f9b67ad54
Rami Abdou <ramiabdou98@gmail.com>
2021-04-28 08:24:42 -0700

    Looking solid.

--- src/scenes/Events/EventsViewRecordingButton.tsx
+++ src/scenes/Events/EventsViewRecordingButton.tsx
@@ -1,17 +1,68 @@
+import { nanoid } from 'nanoid';
 import React from 'react';
 
-import { gql } from '@apollo/client';
+import { DocumentNode, gql, useMutation } from '@apollo/client';
 import Button, { ButtonProps } from '@components/atoms/Button/Button';
 import useMemberRole from '@core/hooks/useMemberRole';
 import useIsMember from '@hooks/useIsMember';
 import { EventTiming, getEventTiming } from '@scenes/Events/Events.util';
 import { ComponentWithFragments } from '@util/constants';
-import { IEvent, MemberRole } from '@util/constants.entities';
+import { IEvent, IEventWatch, MemberRole } from '@util/constants.entities';
+import { now } from '@util/util';
+
+interface CreateEventWatchArgs {
+  createdAt: string;
+  eventId: string;
+  id: string;
+  updatedAt: string;
+}
+
+const CREATE_EVENT_WATCH: DocumentNode = gql`
+  mutation CreateEventWatch(
+    $createdAt: String!
+    $eventId: String!
+    $id: String!
+    $memberId: String
+    $updatedAt: String!
+  ) {
+    memberId @client @export(as: "memberId")
+
+    createEventWatch(
+      object: {
+        createdAt: $createdAt
+        eventId: $eventId
+        id: $id
+        memberId: $memberId
+        updatedAt: $updatedAt
+      }
+      on_conflict: {
+        constraint: event_watches_event_id_member_id_unique
+        update_columns: [updatedAt]
+      }
+    ) {
+      createdAt
+      id
+      updatedAt
+
+      member {
+        firstName
+        id
+        lastName
+        pictureUrl
+      }
+    }
+  }
+`;
 
 const EventsViewRecordingButton: ComponentWithFragments<
   IEvent,
   Partial<Pick<ButtonProps, 'large'>>
 > = ({ data: event, large }) => {
+  const [createEventWatch, { loading }] = useMutation<
+    IEventWatch,
+    CreateEventWatchArgs
+  >(CREATE_EVENT_WATCH);
+
   const isMember: boolean = useIsMember();
   const role: MemberRole = useMemberRole();
 
@@ -28,6 +79,15 @@ const EventsViewRecordingButton: ComponentWithFragments<
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>
   ) => {
     e.stopPropagation();
+
+    await createEventWatch({
+      variables: {
+        createdAt: now(),
+        eventId: event.id,
+        id: nanoid(),
+        updatedAt: now()
+      }
+    });
 
     // await gql.create(IEventWatch, {
     //   data: { eventId, memberId },
@@ -49,6 +109,7 @@ const EventsViewRecordingButton: ComponentWithFragments<
       disabled={!event.recordingUrl}
       href={event.recordingUrl}
       large={large}
+      loading={loading}
       primary={!!event.recordingUrl}
       secondary={!event.recordingUrl}
       onClick={onClick}
@@ -61,6 +122,7 @@ const EventsViewRecordingButton: ComponentWithFragments<
 EventsViewRecordingButton.fragment = gql`
   fragment EventsViewRecordingButtonFragment on events {
     endTime
+    id
     recordingUrl
     startTime
   }
